3319507bf6bdad35d35a8d416920d8f5f98d3769
steverino33 <steve.f@megacatstudios.com>
2023-02-13 08:39:50 -0500

    input button styles

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Meme = () => {
   return (
-    <div className='meme'>
-      <input className='top-text' type="text" name='topText' value={''} />
-      <input className='bottom-text' type="text" name='bottomText' value={''} />
+    <div className="meme">
+      <div className="meme-input">
+        <input
+          className="top-text"
+          type="text"
+          name="topText"
+          value={""}
+          placeholder="Top Text"
+        />
+        <input
+          className="bottom-text"
+          type="text"
+          name="bottomText"
+          value={""}
+          placeholder="Bottom Text"
+        />
+      </div>
+      <div className="meme-button">
+        <button type="button">New Meme Image</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Meme
+export default Meme;
