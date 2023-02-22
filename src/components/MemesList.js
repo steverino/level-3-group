431038d4bbaa469c5ae3f96fb24a7d6289683083
steverino33 <steve.f@megacatstudios.com>
2023-02-22 08:11:37 -0500

    memeslist ul style

--- src/components/MemesList.js
+++ src/components/MemesList.js
@@ -1,18 +1,26 @@
-import React from 'react'
-import Meme from './Meme'
+import React from "react";
+import Meme from "./Meme";
 
-const MemesList = ({topText,image,bottomText,id}) => {
- 
- const [imageList, setImageList] = React.useState([])
-    
-    function memeList(){
-        setImageList([image])
+const MemesList = ({ topText, image, bottomText, id }) => {
+  const [imageList, setImageList] = React.useState([]);
+
+    function editText(){
+        console.log('test');
     }
- return (
-    
-        <Meme key={id} topText={topText} image={image} bottomText={bottomText}/>
-   
-  )
-}
 
-export default MemesList
+  return (
+   <>
+    <div>
+    <Meme key={id} topText={topText} image={image} bottomText={bottomText} />
+    </div>
+    <div>
+        <input type="text" name="editTopText" />
+        <input type="text" name="botomText" />
+        <button type="button" name="editItem" onClick={editText}>Edit</button>
+        <button type="button" name="saveItem">Save Changes</button>
+    </div>
+    </>
+  );
+};
+
+export default MemesList;
